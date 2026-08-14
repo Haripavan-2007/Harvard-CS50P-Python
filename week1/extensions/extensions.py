583c04f04c7f239e6c8a7a2da2ce8e360fb540ca
Haripavan <kiritharanharipavan@gmail.com>
2026-08-14 21:24:03 +0530

    week1 extension

---
+++ week1/extensions/extensions.py
@@ -0,0 +1,18 @@
+filename=input("File name: ")
+sep=filename.strip().split(".")
+extension=sep[-1].lower()
+match extension:
+    case "gif" :
+        print("image/gif")
+    case "jpg"|"jpeg":
+        print("image/jpeg")
+    case "png":
+        print("image/png")
+    case "pdf":
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
